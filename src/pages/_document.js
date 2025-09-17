fc436e10eed89faee2b8bfbf0fbd70ee6a18cd64
Sehrish Jetnetix <sehrishnadeem39@gmail.com>
2025-09-17 16:31:22 +0500

    upadteesd

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,9 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <>
-      
+
       <Html lang="en">
         <Head />
+         <title>Elban</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
